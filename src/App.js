10eec984c87da4e4052a4939a1b372d1f21be2bd
Mayank Verma <mayank.verma1911@gmail.com>
2020-06-22 02:34:49 +0530

    Add Routes; Add Home View; Add Aboutt Us View;

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import HelloWorld from './components/HelloWorld';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+// Import Components
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+// Import Views
+import Home from './views/Home';
+import About from './views/About';
 
 function App() {
   return (
     <div>
-      <Header />
-      
-      <HelloWorld name="Mayank"/>
+      <Router>
 
-      <Footer />
+        <Header />
+        
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+        
+        <Footer />
+
+      </Router>
     </div>
   );
 }
---
+++ src/views/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HelloWorld from '../components/HelloWorld';
+
+
+function Home() {
+  return (
+    <div>
+      <h1>This is home</h1>
+      <HelloWorld name="Mayank"/>
+    </div>
+  );
+}
+
+export default Home;
